9ee1b791d9ce8bbe6129c5b4e6db13f97d5f697d
Nicholas Clarke <nicholas_clarke@xyratex.com>
2014-05-08 10:32:54 +0100

    Fix M0_FOP_TYPE_INIT return (again).

--- mero-ha/hastate/hastate_fops.c
+++ mero-ha/hastate/hastate_fops.c
@@ -33,26 +33,27 @@ int ha_state_fop_init(void)
 {
     m0_xc_note_init();
     m0_xc_note_fops_init();
-    return M0_FOP_TYPE_INIT(&ha_state_get_fopt,
-                            .name      = "M0 State Get",
-                            .opcode    = M0_HA_NOTE_GET_OPCODE,
-                            .xt        = m0_ha_state_fop_get_xc,
-                            .rpc_flags = M0_RPC_ITEM_TYPE_REQUEST,
-                            .fom_ops   = &ha_state_get_fom_type_ops,
-                            .sm        = &m0_generic_conf,
-                            .svc_type  = &m0_rpc_service_type) ?:
-           M0_FOP_TYPE_INIT(&ha_state_get_rep_fopt,
-                            .name      = "HA State Get Reply",
-                            .opcode    = M0_HA_NOTE_GET_REP_OPCODE,
-                            .xt        = m0_ha_state_fop_get_rep_xc,
-                            .rpc_flags = M0_RPC_ITEM_TYPE_REPLY,
-                            .svc_type  = &m0_rpc_service_type) ?:
-           M0_FOP_TYPE_INIT(&ha_state_set_fopt,
-                            .name      = "M0 State Set",
-                            .opcode    = M0_HA_NOTE_SET_OPCODE,
-                            .xt        = m0_ha_nvec_xc,
-                            .rpc_flags = M0_RPC_ITEM_TYPE_REQUEST,
-                            .fom_ops   = &ha_state_set_fom_type_ops,
-                            .sm        = &m0_generic_conf,
-                            .svc_type  = &m0_rpc_service_type);
+    M0_FOP_TYPE_INIT(&ha_state_get_fopt,
+                     .name      = "M0 State Get",
+                     .opcode    = M0_HA_NOTE_GET_OPCODE,
+                     .xt        = m0_ha_state_fop_get_xc,
+                     .rpc_flags = M0_RPC_ITEM_TYPE_REQUEST,
+                     .fom_ops   = &ha_state_get_fom_type_ops,
+                     .sm        = &m0_generic_conf,
+                     .svc_type  = &m0_rpc_service_type);
+    M0_FOP_TYPE_INIT(&ha_state_get_rep_fopt,
+                     .name      = "HA State Get Reply",
+                     .opcode    = M0_HA_NOTE_GET_REP_OPCODE,
+                     .xt        = m0_ha_state_fop_get_rep_xc,
+                     .rpc_flags = M0_RPC_ITEM_TYPE_REPLY,
+                     .svc_type  = &m0_rpc_service_type);
+    M0_FOP_TYPE_INIT(&ha_state_set_fopt,
+                     .name      = "M0 State Set",
+                     .opcode    = M0_HA_NOTE_SET_OPCODE,
+                     .xt        = m0_ha_nvec_xc,
+                     .rpc_flags = M0_RPC_ITEM_TYPE_REQUEST,
+                     .fom_ops   = &ha_state_set_fom_type_ops,
+                     .sm        = &m0_generic_conf,
+                     .svc_type  = &m0_rpc_service_type);
+    return 0;
 }
