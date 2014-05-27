c2d45fe593c9a89eb02fb96a9c2753208ccab0a2
Facundo Domínguez <facundo.dominguez@tweag.io>
2014-05-27 16:42:20 -0300

    Implement path compression.
    EQs now report the node where the RC is. Cohorts then try
    to contact this node first, unless it is not responsive
    in which case it is polled until it responds.

--- ha/tests/HA/NodeAgent/Tests.hs
+++ ha/tests/HA/NodeAgent/Tests.hs
@@ -12,7 +12,7 @@ import HA.Resources (serviceProcess)
 import HA.NodeAgent (nodeAgent,updateEQNodes, Result(..))
 import HA.Network.Address ( Network, getNetworkTransport )
 import HA.Process
-import HA.EventQueue ( eventQueue, EventQueue )
+import HA.EventQueue ( eventQueue, EventQueue, eventQueueLabel )
 import HA.Replicator ( RGroup(..) )
 #ifdef USE_MOCK_REPLICATOR
 import HA.Replicator.Mock ( MC_RG )
@@ -24,23 +24,25 @@ import HA.EventQueue.Producer (expiate)
 import HA.EventQueue.Consumer (HAEvent(..),matchHAEvent)
 
 import Control.Distributed.Process
-         ( Process, spawnLocal, getSelfPid, liftIO,  catch
-         , getSelfNode, say, ProcessId, receiveWait
+         ( Process, spawnLocal, getSelfPid, liftIO,  catch, send, expect
+         , getSelfNode, say, ProcessId, receiveWait, NodeId, register
          )
 #ifndef USE_MOCK_REPLICATOR
-import Control.Distributed.Process ( send, spawn )
+import Control.Distributed.Process ( spawn )
 import Control.Distributed.Static ( closureApply )
+import Control.Distributed.Process.Closure ( mkClosure )
 #endif
-import Control.Distributed.Process.Closure ( mkStatic, mkClosure, remotable )
+import Control.Distributed.Process.Closure ( mkStatic, remotable )
 import Control.Distributed.Process.Node ( LocalNode, localNodeId, newLocalNode, closeLocalNode )
 import Control.Distributed.Process.Internal.Primitives ( unClosure )
 import Control.Distributed.Process.Serializable ( SerializableDict(..) )
 
-import Data.List (find)
+import Data.List (find, nub, (\\))
 import Control.Concurrent ( throwTo, myThreadId, threadDelay )
 import Control.Concurrent.MVar (newEmptyMVar,putMVar,takeMVar,MVar)
-import Control.Monad ( replicateM, forM_ )
-import Control.Exception ( SomeException )
+import Control.Monad ( replicateM, forM_, forever )
+import Control.Exception ( SomeException, bracket )
+import Data.ByteString ( ByteString )
 import System.IO.Unsafe ( unsafePerformIO )
 import Test.Framework
 
@@ -88,8 +90,8 @@ spawnLocalLink f =
   do self <- liftIO myThreadId
      spawnLocal $ flip catch (\e -> liftIO $ throwTo self (e :: SomeException)) $ f
 
-naTest :: Network -> ([LocalNode] -> Process ()) -> IO ()
-naTest network action = withTmpDirectory $ do
+naTestWithEQ :: Network -> ([LocalNode] -> Process ()) -> IO ()
+naTestWithEQ network action = withTmpDirectory $ do
   nodes <- replicateM 3 newNode
   let nids = map localNodeId nodes
   mapM_ (initialize nids) nodes
@@ -122,15 +124,43 @@ naTest network action = withTmpDirectory $ do
       Ok <- updateEQNodes na nids
       return ()
 
+naTest :: Network -> ([NodeId] -> Process ()) -> IO ()
+naTest network action = withTmpDirectory $ bracket
+    (replicateM 2 $ newLocalNode (getNetworkTransport network)
+                                 (__remoteTable remoteTable))
+    (mapM closeLocalNode)
+    $ \nodes -> do
+      let nids = map localNodeId nodes
+      tryRunProcess (nodes !! 0) $ do
+        self <- getSelfPid
+        eq1 <- spawnLocal $ forever $
+                 (expect :: Process (ProcessId, HAEvent [ByteString]))
+                 >>= send self . (,) (nids !! 0)
+        register eventQueueLabel eq1
+        liftIO $ tryRunProcess (nodes !! 1) $ do
+          eq2 <- spawnLocal $ forever $
+                   (expect :: Process (ProcessId, HAEvent [ByteString]))
+                   >>= send self . (,) (nids !! 1)
+          register eventQueueLabel eq2
+        na <- spawnLocalLink =<< unClosure (serviceProcess nodeAgent)
+        Ok <- updateEQNodes na nids
+        action nids
+
+expectEventOnNode :: NodeId -> Process ProcessId
+expectEventOnNode n = do
+    (n', (sender, HAEvent _ _)) <- expect :: Process (NodeId, (ProcessId, HAEvent [ByteString]))
+    True <- return $ n == n'
+    return sender
+
 tests :: Network -> IO [TestTree]
 tests network = do
     return
-      [ testSuccess "rc-get-expiate" $ naTest network $ \_nodes -> do
+      [ testSuccess "rc-get-expiate" $ naTestWithEQ network $ \_nodes -> do
              _ <- spawnLocal $ expiate "hello0"
              liftIO $ takeMVar sync0
              assert True
 
-      , testSuccess "rc-get-expiate-after-eq-death" $ naTest network $ \nodes -> do
+      , testSuccess "rc-get-expiate-after-eq-death" $ naTestWithEQ network $ \nodes -> do
              let getNotMe = do
                      self <- getSelfNode
                      return $ find ((/=) self . localNodeId) nodes
@@ -140,4 +170,77 @@ tests network = do
 
              liftIO $ takeMVar sync1
              assert True
+
+      , testSuccess "na-should-compress-path" $ naTest network $ \nids -> do
+            -- We get an event on the first node.
+            _ <- spawnLocal $ expiate "hello1"
+            sender0 <- expectEventOnNode $ nids !! 0
+            send sender0 (nids !! 0)
+
+            -- We still get an event on the first node and suggest NA to use the
+            -- second node.
+            _ <- spawnLocal $ expiate "hello2"
+            sender1 <- expectEventOnNode $ nids !! 0
+            send sender1 (nids !! 1)
+
+            -- We get an event on the second node.
+            _ <- spawnLocal $ expiate "hello3"
+            sender2 <- expectEventOnNode $ nids !! 1
+            send sender2 (nids !! 1)
+
+            -- We get the next event on the second node again.
+            _ <- spawnLocal $ expiate "hello4"
+            sender3 <- expectEventOnNode $ nids !! 1
+            send sender3 (nids !! 1)
+
+      , testSuccess "na-should-compress-path-with-failures" $ naTest network $ \nids -> do
+            -- We get an event on the first node.
+            _ <- spawnLocal $ expiate "hello1"
+            sender0 <- expectEventOnNode $ nids !! 0
+            send sender0 (nids !! 0)
+
+            -- We still get an event on the first node and suggest NA to use the
+            -- second node.
+            _ <- spawnLocal $ expiate "hello2"
+            sender1 <- expectEventOnNode $ nids !! 0
+            send sender1 (nids !! 1)
+
+            -- We get an event on the second node, but we are not going to
+            -- reply, so NA should resend to the first node.
+            _ <- spawnLocal $ expiate "hello3"
+            _ <- expectEventOnNode $ nids !! 1
+
+            sender2 <- expectEventOnNode $ nids !! 0
+            send sender2 (nids !! 1)
+
+            -- We get an event on the second node and we reply. But because we
+            -- didn't reply last time, NA will send the event to the last
+            -- responsive node, that is the first one.
+            _ <- spawnLocal $ expiate "hello4"
+            evpairs <- replicateM 2 $ (expect :: Process (NodeId, (ProcessId, HAEvent [ByteString])))
+            let nids4 = nub $ map fst evpairs
+                evs   = nub $ map (snd . snd) evpairs
+                senders = map (fst . snd) evpairs
+            -- The same event was sent multiple times.
+            True <- return $ length evs == 1
+            -- The event was sent to both nodes.
+            True <- return $ length nids4 == 2
+            True <- return $ null $ nids4 \\ nids
+            forM_ senders $ \sender -> send sender (nids !! 1)
+
+            -- We get the next event on the second node again, and we suggest
+            -- the first node.
+            _ <- spawnLocal $ expiate "hello5"
+            sender3 <- expectEventOnNode $ nids !! 1
+            send sender3 (nids !! 0)
+
+            -- We get the next event on the first node.
+            _ <- spawnLocal $ expiate "hello6"
+            sender4 <- expectEventOnNode $ nids !! 0
+            send sender4 (nids !! 0)
+
+            -- We get the next event on the first node again.
+            _ <- spawnLocal $ expiate "hello7"
+            sender5 <- expectEventOnNode $ nids !! 0
+            send sender5 (nids !! 0)
       ]
