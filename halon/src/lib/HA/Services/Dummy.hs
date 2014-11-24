6e5510bcc091f68ddadfe30aad3cc26ffc25f461
Nicholas Clarke <nicholas.clarke@seagate.com>
2014-11-24 14:17:08 +0000

    This branch implements reconfiguration messages from halonctl.
    Summary:
    In addition, the following changes are made:
    - Tidy/format a few modules.
    - Remove the old 'reconf' module.
    - Alter the dummy service to take a trivial config parameter. This
    lets us demonstrate that reconfiguration is working.
    
    Test Plan:
    pkill halond
    rm -rf acid-state/
    
    HALOND=./mero-halon/dist/dist-sandbox-39d0ec61/build/halond/halond
    HALONCTL=./mero-halon/dist/dist-sandbox-39d0ec61/build/halonctl/halonctl
    
    $HALOND -l 127.0.0.1:9000 &
    $HALOND -l 127.0.0.1:9100 &
    $HALONCTL -a 127.0.0.1:9100 -a 127.0.0.1:9000 bootstrap satellite
    $HALONCTL -a 127.0.0.1:9100 -a 127.0.0.1:9000 bootstrap station -t 127.0.0.1:9000 -s 127.0.0.1:9100
    sleep 1
    $HALONCTL -a 127.0.0.1:9100 service dummy start -t 127.0.0.1:9000
    sleep 1
    $HALONCTL -a 127.0.0.1:9100 service dummy reconfigure -t 127.0.0.1:9000 --helloWorld "Foobye, World"
    
    Reviewers: mboes
    
    Subscribers: nclarke, facundominguez, mboes
    
    Differential Revision: http://178.62.3.60:8081/D23

--- halon/src/lib/HA/Services/Dummy.hs
+++ halon/src/lib/HA/Services/Dummy.hs
@@ -4,44 +4,132 @@
 --
 -- Should import qualified.
 
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
 
 module HA.Services.Dummy
-    ( dummy
-    , HA.Services.Dummy.__remoteTableDecl ) where
+  ( dummy
+  , DummyConf(..)
+  , HA.Services.Dummy.__remoteTable
+  , HA.Services.Dummy.__remoteTableDecl
+  ) where
 
 import HA.Service
 import HA.NodeAgent
+import HA.ResourceGraph
+import HA.Resources (Node)
 
+import Control.Applicative ((<$>))
 import Control.Distributed.Process
 import Control.Distributed.Process.Closure
 import Control.Distributed.Static
   ( staticApply )
-
 import Control.Concurrent (newEmptyMVar, takeMVar)
+
+import Data.Binary (Binary)
+import Data.Defaultable
+import Data.Hashable (Hashable)
+import Data.Monoid ((<>))
+import Data.Typeable (Typeable)
+
+import GHC.Generics (Generic)
+
+import Options.Schema (Schema)
+import Options.Schema.Builder hiding (name, desc)
+
+newtype DummyConf = DummyConf {
+  helloWorld :: Defaultable String
+} deriving (Binary, Eq, Generic, Hashable, Show, Typeable)
+
+dummySchema :: Schema DummyConf
+dummySchema = let
+    hw = defaultable "Hello World!" . strOption $ long "helloWorld"
+                <> short 'm'
+                <> metavar "GREETING"
+  in DummyConf <$> hw
+
+--------------------------------------------------------------------------------
+-- Dictionaries                                                               --
+--------------------------------------------------------------------------------
+
+dConfigDict :: Dict (Configuration DummyConf)
+dConfigDict = Dict
+
+dSerializableDict :: SerializableDict DummyConf
+dSerializableDict = SerializableDict
+
+--TODO Can we auto-gen this whole section?
+resourceDictServiceDummy :: Dict (Resource (Service DummyConf))
+resourceDictConfigItemDummy :: Dict (Resource DummyConf)
+resourceDictServiceDummy = Dict
+resourceDictConfigItemDummy = Dict
+
+relationDictHasNodeServiceDummy :: Dict (Relation Runs Node (Service DummyConf))
+relationDictWantsServiceDummyConfigItemDummy ::
+  Dict (Relation WantsConf (Service DummyConf) DummyConf)
+relationDictHasServiceDummyConfigItemDummy ::
+  Dict (Relation HasConf (Service DummyConf) DummyConf)
+relationDictHasNodeServiceDummy = Dict
+relationDictWantsServiceDummyConfigItemDummy = Dict
+relationDictHasServiceDummyConfigItemDummy = Dict
+
+remotable
+  [ 'dConfigDict
+  , 'dSerializableDict
+  , 'resourceDictServiceDummy
+  , 'resourceDictConfigItemDummy
+  , 'relationDictHasNodeServiceDummy
+  , 'relationDictHasServiceDummyConfigItemDummy
+  , 'relationDictWantsServiceDummyConfigItemDummy
+  ]
+
+instance Resource (Service DummyConf) where
+  resourceDict = $(mkStatic 'resourceDictServiceDummy)
+
+instance Resource DummyConf where
+  resourceDict = $(mkStatic 'resourceDictConfigItemDummy)
+
+instance Relation Runs Node (Service DummyConf) where
+  relationDict = $(mkStatic 'relationDictHasNodeServiceDummy)
+
+instance Relation HasConf (Service DummyConf) DummyConf where
+  relationDict = $(mkStatic 'relationDictHasServiceDummyConfigItemDummy)
+
+instance Relation WantsConf (Service DummyConf) DummyConf where
+  relationDict = $(mkStatic 'relationDictWantsServiceDummyConfigItemDummy)
+
+--------------------------------------------------------------------------------
+-- End Dictionaries                                                           --
+--------------------------------------------------------------------------------
+
+instance Configuration DummyConf where
+  schema = dummySchema
+  sDict = $(mkStatic 'dSerializableDict)
 
 -- | Block forever.
 never :: Process ()
 never = liftIO $ newEmptyMVar >>= takeMVar
 
 remotableDecl [ [d|
-    dummy :: Service ()
-    dummy = service
-                emptySDict
-                ($(mkStatic 'someConfigDict)
-                    `staticApply` $(mkStatic 'emptyConfigDict))
-                $(mkStatic 'emptySDict)
-                "dummy"
-                $(mkStaticClosure 'dummyProcess)
+  dummy :: Service DummyConf
+  dummy = service
+            dSerializableDict
+            ($(mkStatic 'someConfigDict)
+                `staticApply` $(mkStatic 'dConfigDict))
+            $(mkStatic 'dSerializableDict)
+            "dummy"
+            $(mkStaticClosure 'dummyProcess)
 
-    dummyProcess :: () -> Process ()
-    dummyProcess _ = (`catchExit` onExit) $ do
-        say $ "Starting service dummy"
-        never
-      where
-        onExit _ Shutdown = say $ "DummyService stopped."
-        onExit _ Reconfigure = say $ "DummyService reconfigured."
+  dummyProcess :: DummyConf -> Process ()
+  dummyProcess (DummyConf hw) = (`catchExit` onExit) $ do
+      say $ "Starting service dummy"
+      say . fromDefault $ hw
+      never
+    where
+      onExit _ Shutdown = say $ "DummyService stopped."
+      onExit _ Reconfigure = say $ "DummyService reconfigured."
 
-    |] ]
+  |] ]
