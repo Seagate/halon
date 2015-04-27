56f5b1d5c4a09e6de10afe5bb1b5a81db311427a
Nicholas Clarke <nicholas.clarke@seagate.com>
2015-04-27 13:35:48 +0100

    Start decision-log and frontier in simple cluster.
    These are useful for debugging and testing; we should get used to
    running them by default.

--- mero-halon/scripts/simplecluster.sh
+++ mero-halon/scripts/simplecluster.sh
@@ -3,6 +3,8 @@
 TS=127.0.0.1:9010
 SAT=127.0.0.1:9020
 
+rm -f /tmp/decision.log
+
 HALOND="$HALON_ROOT/halond"
 HALONCTL="$HALON_ROOT/halonctl"
 
@@ -11,3 +13,6 @@ $HALOND -l $SAT > /tmp/satellite.log 2>&1 &
 
 $HALONCTL -a $TS bootstrap station
 $HALONCTL -a $SAT bootstrap satellite -t $TS
+
+$HALONCTL -a $SAT service decision-log start -t $TS -f /tmp/decision.log
+$HALONCTL -a $SAT service frontier start -t $TS -p 9028
