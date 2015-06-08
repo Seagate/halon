7df082625ef9acdb2098975fc4e85f74c98fc62c
Facundo Domínguez <facundo.dominguez@tweag.io>
2015-06-08 09:33:32 -0300

    mero-halon: Test autoboot with more than one TS node.

--- mero-halon/tests/Distributed/autoboot.hs
+++ mero-halon/tests/Distributed/autoboot.hs
@@ -53,9 +53,10 @@ main = do
     Right nt <- createTransport ip "4000" defaultTCPParameters
     n0 <- newLocalNode nt (__remoteTable initRemoteTable)
 
-    withHostNames cp 1 $  \ms@[m0] ->
+    withHostNames cp 2 $  \ms@[m0, m1] ->
      runProcess n0 $ do
       let m0loc = m0 ++ ":9000"
+          m1loc = m1 ++ ":9000"
 
       say "Copying binaries ..."
       -- test copying a folder
@@ -66,22 +67,29 @@ main = do
 
       say "Spawning halond ..."
       nh0 <- spawnNode m0 ("./halond -l " ++ m0loc ++ " 2>&1")
+      nh1 <- spawnNode m1 ("./halond -l " ++ m1loc ++ " 2>&1")
       let nid0 = handleGetNodeId nh0
-      say $ "Redirecting logs from " ++ show nid0 ++ " ..."
+          nid1 = handleGetNodeId nh1
+      say $ "Redirecting logs from " ++ show [nid0, nid1] ++ " ..."
       redirectLogsHere nid0
+      redirectLogsHere nid1
 
       say "Spawning tracking station ..."
       systemThere [m0] ("./halonctl"
                      ++ " -a " ++ m0loc
+                     ++ " -a " ++ m1loc
                      ++ " bootstrap station"
                      )
       expectLog [nid0] (isInfixOf "Recovery Coordinator: New node contacted:")
 
       say "Killing halond"
-      systemThere [m0] "pkill halond; true"
+      systemThere ms "pkill halond; true"
       _ <- liftIO $ waitForCommand_ $ handleGetInput nh0
+      _ <- liftIO $ waitForCommand_ $ handleGetInput nh1
 
       say "Restarting halond"
       nid0' <- spawnNode_ m0 ("./halond -l " ++ m0loc ++ " 2>&1")
+      nid1' <- spawnNode_ m1 ("./halond -l " ++ m1loc ++ " 2>&1")
       redirectLogsHere nid0'
+      redirectLogsHere nid1'
       expectLog [nid0'] (isInfixOf "Found existing graph")
