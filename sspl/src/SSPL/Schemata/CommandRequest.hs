15e956070d79fc8867afe9117b9d0bc47284364a
Nicholas Clarke <nicholas.clarke@seagate.com>
2015-07-02 10:11:57 +0100

    Add security to SSPL-HL schema.

--- sspl/src/SSPL/Schemata/CommandRequest.hs
+++ sspl/src/SSPL/Schemata/CommandRequest.hs
@@ -14,24 +14,50 @@ import Data.Aeson.Schema
 schema = [schemaQQ|
 {
   "$schema":"http://json-schema.org/draft-03/schema#",
-  "type":"object",
+  "type": "object",
   "properties": {
-    "serviceRequest" : {
-      "type" : "object",
-      "properties" : {
-        "serviceName" : {
-          "required" : true,
-          "type" : "string",
-          "description" : "Name of the service to control."
-        },
-        "command" : {
-          "required" : true,
-          "enum" : ["start", "stop", "restart", "enable", "disable", "status"]
-        },
-        "nodes" : {
-          "required" : false,
-          "type" : "string",
-          "description" : "Regex on node FQDNs. If not specified, applies to all nodes."
+    "username": {
+      "description": "Username who generated message",
+      "type": "string",
+      "required": true
+    },
+    "signature": {
+      "description": "Authentication signature of message",
+      "type": "string",
+      "required": true
+    },
+    "time": {
+      "description": "The time the signature was generated",
+      "type": "string",
+      "required": true
+    },
+    "expires": {
+      "description": "The number of secs the signature remains valid after being generated",
+      "type": "integer",
+      "required": false
+    },
+    "message": {
+      "type":"object",
+      "required" : true,
+      "properties": {
+        "serviceRequest" : {
+          "type" : "object",
+          "properties" : {
+            "serviceName" : {
+              "required" : true,
+              "type" : "string",
+              "description" : "Name of the service to control."
+            },
+            "command" : {
+              "required" : true,
+              "enum" : ["start", "stop", "restart", "enable", "disable", "status"]
+            },
+            "nodes" : {
+              "required" : false,
+              "type" : "string",
+              "description" : "Regex on node FQDNs. If not specified, applies to all nodes."
+            }
+          }
         }
       }
     }
