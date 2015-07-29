6201e42818910bfa8047eb0aa6a4141a3af280ab
Alexander Vershilov <alexander.vershilov@gmail.com>
2015-07-29 18:30:13 +0300

    network-transport-controlled: add a way to recover connectivity.

--- network-transport-controlled/src/Network/Transport/Controlled.hs
+++ network-transport-controlled/src/Network/Transport/Controlled.hs
@@ -10,6 +10,7 @@
 module Network.Transport.Controlled
   ( createTransport
   , silenceBetween
+  , unsilence
   , Controlled
   ) where
 
@@ -50,6 +51,16 @@ silenceBetween (Controlled t mBrokenLinks break) a b = do
    modifyMVar_ mBrokenLinks $ \m -> do
      break a b
      return $ Map.alter (Just . (a:) . fromMaybe []) b (Map.alter (Just . (b:) . fromMaybe []) a m)
+
+-- | Remove persistent breakage between nodes, this means that any following
+-- call to 'connect' will succeed. Semantics of the call to 'send' depends on
+-- 'break' function that was passed to 'createTransport', if it closed underlying
+-- connection then 'send' will fail, otherwise following 'send' will succeed.
+unsilence :: Controlled -> EndPointAddress -> EndPointAddress -> IO ()
+unsilence (Controlled t mBrokenLinks _) a b = do
+   modifyMVar_ mBrokenLinks $ \m -> do
+     return $ Map.alter (Just . filter (/=a) . fromMaybe []) b
+                        (Map.alter (Just . filter (/=b) . fromMaybe []) a m)
 
 apiNewEndPoint :: Controlled -> IO (Either (TransportError NewEndPointErrorCode) EndPoint)
 apiNewEndPoint c@(Controlled transport brokenLinks _) = fmap wrapEndPoint <$> newEndPoint transport where
