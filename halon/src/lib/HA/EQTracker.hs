bec591d386b9174eb3cb2f786b415b3ec4827c93
Alexander Vershilov <alexander.vershilov@gmail.com>
2015-08-04 20:01:45 +0300

    Remove EQT from beign a service.
    Because event queue is required process on the node
    and have no configuration it makes no sense to make
    it a service. Because making it a service introduces
    a huge window for additional failures and race conditions
    and forces higher level libraries implement bootstrap
    procedure, that lead to a mixture of different layers.

--- halon/src/lib/HA/EQTracker.hs
+++ halon/src/lib/HA/EQTracker.hs
@@ -4,8 +4,8 @@
 --
 -- Should import qualified.
 --
--- Service to track the location of (a subset of) EventQueue nodes. This
--- simplifies expediting messages to the Recovery Co-ordinator, since each
+-- EventQueueTracker intended to to track the location of (a subset of) EventQueue
+-- nodes. This simplifies expediting messages to the Recovery Co-ordinator, since each
 -- individual service need not track the location of the EventQueue nodes.
 --
 -- Note that the EQTracker does not act as a proxy for the EventQueue, it just
@@ -18,24 +18,21 @@
 
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
 
-module HA.Services.EQTracker
+module HA.EQTracker
   ( ReplicaLocation(..)
   , ReplicaRequest(..)
   , ReplicaReply(..)
   , PreferReplicas(..)
-  , eqTracker
+  , eqTrackerProcess
   , name
-  , HA.Services.EQTracker.__remoteTableDecl
+  , HA.EQTracker.__remoteTableDecl
   ) where
 
 import HA.NodeAgent.Messages (ServiceMessage(UpdateEQNodes))
-import HA.Service
-import HA.Services.Empty
 
+import Control.SpineSeq (spineSeq)
 import Control.Distributed.Process hiding (send)
 import Control.Distributed.Process.Closure
-import Control.Distributed.Static
-  ( staticApply )
 
 import Data.Binary (Binary)
 import Data.Hashable (Hashable)
@@ -43,6 +40,7 @@ import Data.List (intersect, sort)
 import Data.Typeable (Typeable)
 
 import GHC.Generics (Generic)
+import Text.Printf
 
 -- | Loop state for the tracker. We store a list of preferred replicas as
 --   well as the full list of known replicas. None of these are guaranteed
@@ -75,31 +73,38 @@ newtype ReplicaReply = ReplicaReply ReplicaLocation
 name :: String
 name = "HA.EQTracker"
 
+
 remotableDecl [ [d|
 
-  eqTrackerProcess :: EmptyConf -> Process ()
-  eqTrackerProcess _ = do
+  -- | Run Event Queue process.
+  --
+  -- This process is not intended to fail, in because if this process
+  -- fails then there is no way to reliably send messages to the EQ.
+  eqTrackerProcess :: [NodeId] -> Process ()
+  eqTrackerProcess nodes = do
       self <- getSelfPid
       ensureRegistered self
-      go $ ReplicaLocation [] []
+      go $ ReplicaLocation [] nodes
     where
       ensureRegistered pid = do
         exists <- whereis name
         case exists of
-          Just _ -> reregister name pid
+          Just old -> do
+            -- XXX: announce the change, ideally we should register ourself on RC
+            say $ printf "EQTracker: reregistering %s (old EQT was %s)" name (show old)
+            reregister name pid
           Nothing -> register name pid
       go eqs = do
         receiveWait
           [ match $ \(caller, UpdateEQNodes eqnids) -> do
               say $ "Got UpdateEQNodes: " ++ show eqnids
               usend caller True
+              let !preferred = spineSeq $ intersect eqnids (eqsPreferredReplicas eqs)
               return $ eqs
                 { eqsReplicas = eqnids
                   -- Preserve the preferred replica only if it belongs
                   -- to the new list of nodes.
-                , eqsPreferredReplicas = intersect
-                    eqnids
-                    (eqsPreferredReplicas eqs)
+                , eqsPreferredReplicas = preferred
                 }
           , match $ \prs@(PreferReplicas rnid pnid) -> do
               say $ "Got PreferReplicas: " ++ show prs
@@ -108,13 +113,6 @@ remotableDecl [ [d|
               usend requester $ ReplicaReply eqs
               return eqs
           ] >>= go
-
-  eqTracker :: Service EmptyConf
-  eqTracker = Service
-                (ServiceName name)
-                ($(mkStaticClosure 'eqTrackerProcess))
-                ($(mkStatic 'someConfigDict)
-                  `staticApply` $(mkStatic 'configDictEmptyConf))
 
   -- The EQ response may suggest to contact another replica. This function
   -- handles the EQTracker state update.
@@ -125,16 +123,13 @@ remotableDecl [ [d|
   handleEQResponse eqs rnid pnid =
     if sort [rnid, pnid] /= sort (eqsPreferredReplicas eqs)
     then
-      updateEQS eqs $
-        if rnid == pnid
-        then [rnid] -- The EQ sugested itself.
-        else if rnid `elem` (eqsPreferredReplicas eqs)
-          then [pnid] -- Likely we have not tried reaching the preferred node yet.
-          else [rnid, pnid] -- The preferred replica did not respond soon enough.
+      let !preferred = if rnid == pnid
+                       then [rnid] -- The EQ sugested itself.
+                       else if rnid `elem` (eqsPreferredReplicas eqs)
+                            then [pnid] -- Likely we have not tried reaching the preferred node yet.
+                            else [rnid, pnid] -- The preferred replica did not respond soon enough.
+      in eqs{ eqsPreferredReplicas = preferred }
     else
       eqs
-
-  updateEQS :: ReplicaLocation -> [NodeId] -> ReplicaLocation
-  updateEQS eqs pnids = eqs { eqsPreferredReplicas = pnids }
 
   |] ]
