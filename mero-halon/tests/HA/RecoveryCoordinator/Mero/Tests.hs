c46729c465320cf612e3f8182bb0acf0cbc2733d
Alexander Vershilov <alexander.vershilov@gmail.com>
2015-08-07 19:42:59 +0300

    fixup! Port mero-halon to new EQT API.

--- mero-halon/tests/HA/RecoveryCoordinator/Mero/Tests.hs
+++ mero-halon/tests/HA/RecoveryCoordinator/Mero/Tests.hs
@@ -610,7 +610,6 @@ testNodeUpRace transport = do
         self <- getSelfPid
         _ <- spawnLocal $ eqTrackerProcess [nid]
 
-
         say $ "tests node: " ++ show nid
         cRGroup <- newRGroup $(mkStatic 'testDict) 1000 1000000
                              [nid] ((Nothing,[]), fromList [])
@@ -624,6 +623,7 @@ testNodeUpRace transport = do
         liftIO $ do
           node2 <- newLocalNode transport rt
           runProcess node2 $ do
+            _ <- spawnLocal $ eqTrackerProcess [nid]
             selfNode <- getSelfNode
             _ <- promulgateEQ [nid] . encodeP $ ServiceStarted (Node selfNode)
                                                                EQT.eqTracker
