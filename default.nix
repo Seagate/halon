417c6b0af2fb2e4b3f6f43431ff5cf61ebd03349
Nicholas Clarke <nicholas.clarke@seagate.com>
2015-08-17 16:12:25 +0100

    Remove all sodium related things.

--- default.nix
+++ default.nix
@@ -3,18 +3,16 @@ let
   stdenv      = pkgs.stdenv;
   hpkgs       = pkgs.haskellPackages_ghc783;
   cep         = import ./cep hpkgs;
-  sodiumUtils = import ./sodium-utils { inherit (hpkgs) cabal time sodium unboundedDelays; };
   gloss       = hpkgs.gloss.override { bmp = hpkgs.bmp.override { binary = hpkgs.binary; }; };
-  cepSodium   = import ./cep-sodium (hpkgs // { inherit cep sodiumUtils gloss; });
 in
   stdenv.mkDerivation {
     name        = "tweag-cep";
     src         = ./.;
     version     = "0.1.0.0";
     buildInputs = [
-      cep cepSodium sodiumUtils
+      cep
 
-      hpkgs.networkTransportTcp hpkgs.random hpkgs.stm hpkgs.multimap hpkgs.lens hpkgs.distributedProcess hpkgs.sodium gloss
+      hpkgs.networkTransportTcp hpkgs.random hpkgs.stm hpkgs.multimap hpkgs.lens hpkgs.distributedProcess gloss
       
       hpkgs.ghc hpkgs.cabalInstall pkgs.git pkgs.less pkgs.openssh pkgs.gv pkgs.glibc
     ];
