2bcc519739db71f9643a30bd6be81d6ec4aee4cd
Nicholas Clarke <nicholas.clarke@seagate.com>
2015-08-18 15:29:22 +0100

    Update SM.
    - Remove unnecessary third argument to SM_Out.
    - Rename 'SM' related functions to better match what they do.

--- cep/src/Network/CEP/SM.hs
+++ cep/src/Network/CEP/SM.hs
@@ -22,19 +22,17 @@ data PhaseBufAction = CopyThatBuffer Buffer | CreateNewBuffer
 
 data SpawnSM g l = SpawnSM PhaseBufAction l (PhaseM g l ())
 
-data SM_Exe
-
 data SM_In g l a where
     PushMsg :: Typeable m => m -> SM_In g l ()
-    Execute :: Subscribers -> g -> (Phase g l) -> SM_In g l SM_Exe
+    Execute :: Subscribers -> g -> (Phase g l) -> SM_In g l a
 
-data SM_Out g l a where
-    SM_Complete :: g -> l -> [SpawnSM g l] -> [PhaseHandle] -> Maybe SMLogs -> SM_Out g l SM_Exe
-    SM_Suspend  :: Maybe SMLogs -> SM_Out g l SM_Exe
-    SM_Stop     :: Maybe SMLogs -> SM_Out g l SM_Exe
-    SM_Unit     :: SM_Out g l ()
+data SM_Out g l where
+    SM_Complete :: g -> l -> [SpawnSM g l] -> [PhaseHandle] -> Maybe SMLogs -> SM_Out g l
+    SM_Suspend  :: Maybe SMLogs -> SM_Out g l
+    SM_Stop     :: Maybe SMLogs -> SM_Out g l
+    SM_Unit     :: SM_Out g l
 
-smLocalState :: SM_Out g l a -> Maybe l
+smLocalState :: SM_Out g l -> Maybe l
 smLocalState (SM_Complete _ l _ _ _) = Just l
 smLocalState _                       = Nothing
 
@@ -49,10 +47,10 @@ notifySubscribers subs a = do
 -- | Phase Mealy finite state machine.
 newtype SM g l =
     SM { unSM :: forall a. SM_In g l a
-              -> Process (Buffer, Buffer, SM_Out g l a, SM g l) }
+              -> Process (Buffer, Buffer, SM_Out g l, SM g l) }
 
 newSM :: Buffer -> Maybe SMLogs -> l -> SM g l
-newSM buf logs l = SM $ mainSM buf logs l
+newSM buf logs l = SM $ runPhase buf logs l
 
 -- | Simple product type used as a result of message buffer extraction.
 data Extraction b =
@@ -144,32 +142,32 @@ extractSeqMsg s sbuf = go (-1) sbuf s
 -- | Execute a 'Phase' state machine. If it's 'DirectCall' 'Phase', it's runned
 --   directly. If it's 'ContCall' one, we make sure we can satisfy its
 --   dependency. Otherwise, we 'Suspend' that phase.
-mainSM :: Buffer
-       -> Maybe SMLogs
-       -> l
-       -> SM_In g l a
-       -> Process (Buffer, Buffer, SM_Out g l a, SM g l)
-mainSM buf logs l (PushMsg msg) =
+runPhase :: Buffer
+         -> Maybe SMLogs
+         -> l
+         -> SM_In g l a
+         -> Process (Buffer, Buffer, SM_Out g l, SM g l)
+runPhase buf logs l (PushMsg msg) =
     let new_buf = bufferInsert msg buf in
-    return (buf, new_buf, SM_Unit, SM $ mainSM new_buf logs l)
-mainSM buf logs l (Execute subs g ph) =
+    return (buf, new_buf, SM_Unit, SM $ runPhase new_buf logs l)
+runPhase buf logs l (Execute subs g ph) =
     case _phCall ph of
       DirectCall action -> do
-        (new_buf, out) <- runSM (_phName ph) subs buf g l [] logs action
+        (new_buf, out) <- runPhaseM (_phName ph) subs buf g l [] logs action
         let final_buf =
               case out of
                 SM_Suspend{} -> buf
                 SM_Stop{}    -> buf
                 _            -> new_buf
             nxt_l = fromMaybe l $ smLocalState out
-        return (buf, final_buf, out, SM $ mainSM final_buf logs nxt_l)
+        return (buf, final_buf, out, SM $ runPhase final_buf logs nxt_l)
       ContCall tpe k -> do
         res <- extractMsg tpe g l buf
         case res of
           Just (Extraction new_buf b) -> do
             notifySubscribers subs b
             let name = _phName ph
-            (lastest_buf, out) <- runSM name subs new_buf g l [] logs (k b)
+            (lastest_buf, out) <- runPhaseM name subs new_buf g l [] logs (k b)
             let final_buf =
                   case out of
                     SM_Suspend{} -> buf
@@ -179,30 +177,30 @@ mainSM buf logs l (Execute subs g ph) =
             case out of
               SM_Complete{} -> notifySubscribers subs b
               _             -> return ()
-            return (buf, final_buf, out, SM $ mainSM final_buf logs nxt_l)
-          Nothing -> return (buf, buf, SM_Suspend Nothing, SM $ mainSM buf logs l)
+            return (buf, final_buf, out, SM $ runPhase final_buf logs nxt_l)
+          Nothing -> return (buf, buf, SM_Suspend Nothing, SM $ runPhase buf logs l)
 
 -- | 'Phase' state machine execution main loop. Runs until its stack is empty
 --   except if get a 'Suspend' or 'Stop' instruction.
-runSM :: String
-      -> Subscribers
-      -> Buffer
-      -> g
-      -> l
-      -> [SpawnSM g l]
-      -> Maybe SMLogs
-      -> PhaseM g l ()
-      -> Process (Buffer, SM_Out g l SM_Exe)
-runSM pname subs buf g l stk logs action = viewT action >>= go
+runPhaseM :: String
+          -> Subscribers
+          -> Buffer
+          -> g
+          -> l
+          -> [SpawnSM g l]
+          -> Maybe SMLogs
+          -> PhaseM g l ()
+          -> Process (Buffer, SM_Out g l)
+runPhaseM pname subs buf g l stk logs action = viewT action >>= go
   where
     go (Return _) = return (buf, SM_Complete g l (reverse stk) [] logs)
     go (Continue ph :>>= _) = return (buf, SM_Complete g l (reverse stk) [ph] logs)
-    go (Save s :>>= k) = runSM pname subs buf s l stk logs $ k ()
-    go (Load :>>= k) = runSM pname subs buf g l stk logs $ k g
-    go (Get Global :>>= k) = runSM pname subs buf g l stk logs $ k g
-    go (Get Local :>>= k) = runSM pname subs buf g l stk logs $ k l
-    go (Put Global s :>>= k) = runSM pname subs buf s l stk logs $ k ()
-    go (Put Local l' :>>= k) = runSM pname subs buf g l' stk logs $ k ()
+    go (Save s :>>= k) = runPhaseM pname subs buf s l stk logs $ k ()
+    go (Load :>>= k) = runPhaseM pname subs buf g l stk logs $ k g
+    go (Get Global :>>= k) = runPhaseM pname subs buf g l stk logs $ k g
+    go (Get Local :>>= k) = runPhaseM pname subs buf g l stk logs $ k l
+    go (Put Global s :>>= k) = runPhaseM pname subs buf s l stk logs $ k ()
+    go (Put Local l' :>>= k) = runPhaseM pname subs buf g l' stk logs $ k ()
     go (Stop :>>= _) = return (buf, SM_Stop logs)
     go (Fork typ naction :>>= k) =
         let bufAction =
@@ -211,23 +209,23 @@ runSM pname subs buf g l stk logs action = viewT action >>= go
                 CopyBuffer -> CopyThatBuffer buf
 
             ssm = SpawnSM bufAction l naction in
-        runSM pname subs buf g l (ssm : stk) logs $ k ()
+        runPhaseM pname subs buf g l (ssm : stk) logs $ k ()
     go (Lift m :>>= k) = do
         a <- m
-        runSM pname subs buf g l stk logs $ k a
+        runPhaseM pname subs buf g l stk logs $ k a
     go (Suspend :>>= _) = return (buf, SM_Suspend logs)
     go (Publish e :>>= k) = do
         notifySubscribers subs e
-        runSM pname subs buf g l stk logs $ k ()
+        runPhaseM pname subs buf g l stk logs $ k ()
     go (PhaseLog ctx lg :>>= k) =
         let new_logs = fmap (S.|> (pname,ctx,lg)) logs in
-        runSM pname subs buf g l stk new_logs $ k ()
+        runPhaseM pname subs buf g l stk new_logs $ k ()
     go (Switch xs :>>= _) = return (buf, SM_Complete g l (reverse stk) xs logs)
     go (Peek idx :>>= k) = do
         case bufferPeek idx buf of
           Nothing -> return (buf, SM_Suspend logs)
-          Just r  -> runSM pname subs buf g l stk logs $ k r
+          Just r  -> runPhaseM pname subs buf g l stk logs $ k r
     go (Shift idx :>>= k) =
         case bufferGetWithIndex idx buf of
           (Nothing, _)   -> return (buf, SM_Suspend logs)
-          (Just r, buf') -> runSM pname subs buf' g l stk logs $ k r
+          (Just r, buf') -> runPhaseM pname subs buf' g l stk logs $ k r
