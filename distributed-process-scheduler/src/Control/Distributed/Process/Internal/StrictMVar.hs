f2c4eef356e3cdee7b3b57ff51793f415b8f2bb5
Facundo Domínguez <facundo.dominguez@tweag.io>
2015-08-27 12:50:04 -0300

    d-p-scheduler: Reexport C.D.P.Internal.StrictMVar.

---
+++ distributed-process-scheduler/src/Control/Distributed/Process/Internal/StrictMVar.hs
@@ -0,0 +1,9 @@
+-- |
+-- Copyright : (C) 2015 Seagate Technology Limited.
+-- License   : All rights reserved.
+
+{-# LANGUAGE PackageImports #-}
+module Control.Distributed.Process.Internal.StrictMVar
+  ( module DP ) where
+
+import "distributed-process" Control.Distributed.Process.Internal.StrictMVar as DP
