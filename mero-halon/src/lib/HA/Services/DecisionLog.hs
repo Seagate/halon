e33dce0461c497cf3b52c3fa84550666df7b8b54
YoEight <yo.eight@gmail.com>
2015-09-07 20:56:01 +0200

    Implement Decision Log printing on different outputs

--- mero-halon/src/lib/HA/Services/DecisionLog.hs
+++ mero-halon/src/lib/HA/Services/DecisionLog.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE Rank2Types      #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Rank2Types        #-}
+{-# LANGUAGE TemplateHaskell   #-}
 -- |
 -- Copyright: (C) 2015 Tweag I/O Limited
 --
@@ -9,6 +10,9 @@ module HA.Services.DecisionLog
     , decisionLog
     , decisionLogService
     , decisionLogServiceName
+    , fileOutput
+    , processOutput
+    , standardOutput
     , HA.Services.DecisionLog.Types.__remoteTable
     , __remoteTableDecl
     , decisionLogService__sdict
@@ -18,16 +22,18 @@ module HA.Services.DecisionLog
 
 import System.IO
 
-import Control.Distributed.Process
-import Control.Distributed.Process.Closure
-import Control.Distributed.Static
-import Network.CEP
+import           Control.Distributed.Process
+import           Control.Distributed.Process.Closure
+import           Control.Distributed.Static
+import qualified Data.ByteString.Lazy as Lazy
+import           Data.ByteString.Lazy.Char8 (pack)
+import           Network.CEP
 
 import HA.Service
 import HA.Services.DecisionLog.CEP
 import HA.Services.DecisionLog.Types
 
-makeDecisionLogProcess :: (forall s. Definitions s ()) -> Process ()
+makeDecisionLogProcess ::  Definitions () () -> Process ()
 makeDecisionLogProcess rules = execute () rules
 
 decisionLogServiceName :: ServiceName
@@ -35,6 +41,19 @@ decisionLogServiceName = ServiceName "decision-log"
 
 cleanupHandle :: Handle -> Process ()
 cleanupHandle h = liftIO $ hClose h
+
+mkWriteLogs :: DecisionLogOutput -> (WriteLogs -> Process ()) -> Process ()
+mkWriteLogs (FileOutput path) k =
+    bracket (openLogFile path) cleanupHandle $ \h -> do
+      let wl = WriteLogs $ \logs -> liftIO $ do
+            Lazy.hPut h $ pack $ show logs
+            Lazy.hPut h "\n"
+
+      k wl
+mkWriteLogs (ProcessOutput pid) k =
+    k $ WriteLogs (usend pid)
+mkWriteLogs StandardOutput k =
+    let wl = WriteLogs $ \logs -> liftIO $ putStrLn $ show logs in k wl
 
 openLogFile :: FilePath -> Process Handle
 openLogFile path = liftIO $ do
@@ -44,9 +63,8 @@ openLogFile path = liftIO $ do
 
 remotableDecl [ [d|
     decisionLogService :: DecisionLogConf -> Process ()
-    decisionLogService (DecisionLogConf path) =
-        bracket (openLogFile path) cleanupHandle $ \h ->
-          makeDecisionLogProcess $ decisionLogRules h
+    decisionLogService (DecisionLogConf out) =
+        mkWriteLogs out (makeDecisionLogProcess . decisionLogRules)
 
     decisionLog :: Service DecisionLogConf
     decisionLog = Service
--- mero-halon/src/lib/HA/Services/DecisionLog/Types.hs
+++ mero-halon/src/lib/HA/Services/DecisionLog/Types.hs
@@ -9,6 +9,7 @@
 module HA.Services.DecisionLog.Types where
 
 import           Prelude hiding ((<$>))
+import           Control.Applicative ((<|>))
 import qualified Data.ByteString      as Strict
 import qualified Data.ByteString.Lazy as Lazy
 import           Data.Functor ((<$>))
@@ -16,16 +17,42 @@ import           Data.Monoid ((<>))
 import           Data.Typeable
 import           GHC.Generics
 
+import Control.Distributed.Process (ProcessId, Process)
 import Data.Binary
 import Data.Hashable
 import Options.Schema
 import Options.Schema.Builder
+import Network.CEP
 
 import HA.Service.TH
 
-data DecisionLogConf =
-    DecisionLogConf
-    { _dlcFilePath :: String }
+data DecisionLogOutput
+    = FileOutput FilePath
+      -- ^ File path of a file.
+    | ProcessOutput ProcessId
+      -- ^ Sends any log to the specified 'Process'.
+    | StandardOutput
+      -- ^ Writes to stdout.
+    deriving (Eq, Show, Generic)
+
+instance Binary DecisionLogOutput
+instance Hashable DecisionLogOutput
+
+-- | Writes any log to a file. It will append the content from the end of the
+--   file.
+fileOutput :: FilePath -> DecisionLogOutput
+fileOutput = FileOutput
+
+-- | Sends any log to that 'Process'.
+processOutput :: ProcessId -> DecisionLogOutput
+processOutput = ProcessOutput
+
+
+-- | Writes any log to stdout.
+standardOutput :: DecisionLogOutput
+standardOutput = StandardOutput
+
+newtype DecisionLogConf = DecisionLogConf  DecisionLogOutput
     deriving (Eq, Generic, Show, Typeable)
 
 instance Binary DecisionLogConf
@@ -37,7 +64,7 @@ decisionLogSchema =
                    $  long "file"
                    <> short 'f'
                    <> metavar "DECISION_LOG_FILE" in
-     DecisionLogConf <$> filepath
+     DecisionLogConf <$> ((FileOutput <$> filepath) <|> pure StandardOutput)
 
 $(generateDicts ''DecisionLogConf)
 $(deriveService ''DecisionLogConf 'decisionLogSchema [])
@@ -50,3 +77,8 @@ data EntriesLogged =
     } deriving (Generic, Typeable)
 
 instance Binary EntriesLogged
+
+newtype WriteLogs = WriteLogs (Logs -> Process ())
+
+writeLogs :: WriteLogs -> Logs -> Process ()
+writeLogs (WriteLogs k) logs = k logs
