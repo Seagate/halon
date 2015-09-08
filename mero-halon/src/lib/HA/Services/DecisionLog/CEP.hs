5f7d841ac6151ab417a9ea2e44a351b00f7ca7dc
YoEight <yo.eight@gmail.com>
2015-09-08 14:16:25 +0200

    Fix warnings

--- mero-halon/src/lib/HA/Services/DecisionLog/CEP.hs
+++ mero-halon/src/lib/HA/Services/DecisionLog/CEP.hs
@@ -4,10 +4,7 @@
 --
 module HA.Services.DecisionLog.CEP where
 
-import  System.IO
-
 import Control.Distributed.Process
-import Control.Monad.Trans
 import Network.CEP
 
 import HA.Services.DecisionLog.Types
