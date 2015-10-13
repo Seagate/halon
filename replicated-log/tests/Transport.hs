d9553cf11ee74a19d8b5860068753b188b67ccab
Facundo Domínguez <facundo.dominguez@tweag.io>
2015-10-13 06:58:50 -0300

    replicated-log: Have connectionClose not fail on failed connections.

--- replicated-log/tests/Transport.hs
+++ replicated-log/tests/Transport.hs
@@ -8,7 +8,9 @@ module Transport
   ) where
 
 import Network.Transport
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (threadDelay, forkIO)
+import Control.Concurrent.MVar
+import Control.Exception
 import qualified Network.Socket as N (close)
 import qualified Network.Transport.TCP as TCP
 import qualified Network.Transport.InMemory as InMemory
@@ -21,12 +23,20 @@ data AbstractTransport = AbstractTransport
 
 mkTCPTransport :: IO AbstractTransport
 mkTCPTransport = do
-  Right (transport, internals) <- TCP.createTransportExposeInternals "127.0.0.1" "0" TCP.defaultTCPParameters
-  let closeConnection here there = do
-        TCP.socketBetween internals here there >>= N.close
-        TCP.socketBetween internals there here >>= N.close
-        threadDelay 1000000
-  return (AbstractTransport transport closeConnection (closeTransport transport))
+    Right (transport, internals) <- TCP.createTransportExposeInternals "127.0.0.1" "0" TCP.defaultTCPParameters
+    let -- XXX: Could use enclosed-exceptions here. Note that the worker
+        -- is not killed in case of an exception.
+        ignoreSyncExceptions action = do
+          mv <- newEmptyMVar
+          _ <- forkIO $ action `finally` putMVar mv ()
+          takeMVar mv
+        closeConnection here there = do
+          ignoreSyncExceptions $
+            TCP.socketBetween internals here there >>= N.close
+          ignoreSyncExceptions $
+            TCP.socketBetween internals there here >>= N.close
+          threadDelay 1000000
+    return (AbstractTransport transport closeConnection (closeTransport transport))
 
 mkInMemoryTransport :: IO AbstractTransport
 mkInMemoryTransport = do
