59310a5236c029ea3533f6ab0d050ff5e2abe0a6
Nicholas Clarke <nicholas.clarke@seagate.com>
2015-10-13 11:04:12 +0100

    Include 'verify' option for initial data parsing.

--- mero-halon/src/halonctl/Handler/Cluster.hs
+++ mero-halon/src/halonctl/Handler/Cluster.hs
@@ -14,28 +14,45 @@ import HA.EventQueue.Producer (promulgateEQ)
 import qualified HA.Resources.Castor.Initial as CI
 
 import Control.Distributed.Process
+import Control.Monad (void)
 
-import Data.Aeson (decode)
+import Data.Yaml
+  ( decodeFileEither
+  , prettyPrintParseException
+  , ParseException
+  , Value (..)
+  )
 import qualified Data.ByteString.Lazy as BS
 import Options.Applicative ((<>))
 import qualified Options.Applicative as Opt
 
-newtype ClusterOptions = ClusterOptions FilePath
+data ClusterOptions = ClusterOptions
+    FilePath
+    Bool -- ^ validate only
   deriving (Eq, Show)
 
 parseCluster :: Opt.Parser ClusterOptions
-parseCluster = ClusterOptions <$>
-             ( Opt.strOption
-             $ Opt.long "conffile"
-            <> Opt.short 'f'
-            <> Opt.help "File containing JSON-encoded configuration."
-            <> Opt.metavar "FILEPATH"
-             )
+parseCluster = ClusterOptions
+  <$> Opt.strOption
+      ( Opt.long "conffile"
+     <> Opt.short 'f'
+     <> Opt.help "File containing JSON-encoded configuration."
+     <> Opt.metavar "FILEPATH"
+      )
+  <*> Opt.switch
+      ( Opt.long "verify"
+     <> Opt.short 'v'
+     <> Opt.help "Verify config file without reconfiguring cluster."
+      )
 
 dataLoad :: [NodeId] -- ^ EQ nodes to send data to
          -> ClusterOptions
          -> Process ()
-dataLoad eqnids (ClusterOptions cf) = do
-  bs <- liftIO $ BS.readFile cf
-  mapM_ (promulgateEQ eqnids) (decode bs :: Maybe CI.InitialData)
-
+dataLoad eqnids (ClusterOptions cf verify) = do
+  initData <- liftIO $ decodeFileEither cf
+  case initData of
+    Left err -> liftIO . putStrLn $ prettyPrintParseException err
+    Right datum | verify -> liftIO $ do
+      putStrLn "Initial data file parsed successfully."
+      print datum
+    Right (datum :: CI.InitialData) -> void $ promulgateEQ eqnids datum
