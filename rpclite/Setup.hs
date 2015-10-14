42c4b2e8d75538fd8f77dce7e14814d636609190
Ben Clifford <benc@hawaga.org.uk>
2015-10-14 10:32:00 +0100

    ff2c -> m0ff2c rename, when looking on the system path rather than under MERO_ROOT

--- rpclite/Setup.hs
+++ rpclite/Setup.hs
@@ -20,7 +20,7 @@ rpclite_build pd lbi uh bf@(BuildFlags { buildVerbosity = vf }) = do
   mero_root <- lookupEnv "MERO_ROOT"
   let v = fromFlagOrDefault normal vf
       ff2c = fmap (++ "/xcode/ff2c/m0ff2c") mero_root
-      progdb = userMaybeSpecifyPath "ff2c" ff2c $ withPrograms lbi
-  (prog, _) <- requireProgram v (simpleProgram "ff2c") progdb
+      progdb = userMaybeSpecifyPath "m0ff2c" ff2c $ withPrograms lbi
+  (prog, _) <- requireProgram v (simpleProgram "m0ff2c") progdb
   rawSystemProgram v prog ["rpclite/rpclite_fop.ff"]
   (buildHook simpleUserHooks) pd lbi uh bf
