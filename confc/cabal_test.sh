f90865ab5a99e5aaf07580406c4d9d2eaa2255c7
Alexander Vershilov <alexander.vershilov@tweag.io>
2015-10-30 10:07:10 +0000

    Update confc tests.

--- confc/cabal_test.sh
+++ confc/cabal_test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -ex
 CONFC_TOP=`pwd`
-BUILD_DIR=$1
+BUILD_DIR=${CONFC_TOP}/$1
 TEST_DIR=$(dirname $BUILD_DIR)/test
 
 mkdir -p $TEST_DIR
 cd $TEST_DIR
 
-BUILD_DIR=../build
 SUDO="sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 $SUDO sh -c \
-   "( cd /tmp && $BUILD_DIR/testconf/testconf `sudo lctl list_nids | head -1`:12345:35:401 `sudo lctl list_nids | head -1`:12345:41:101 )" > testconfc_out.txt
+   "( cd /tmp && $BUILD_DIR/testconf/testconf `sudo lctl list_nids | head -1`:12345:35:401 `sudo lctl list_nids | head -1`:12345:44:101 )" > testconfc_out.txt
