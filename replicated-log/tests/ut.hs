800c72dc4b82d333b25e2c1951d61697379ee59b
Facundo Domínguez <facundo.dominguez@tweag.io>
2015-11-05 13:04:15 -0300

    replicated-log: Make easy disabling tasty-files in unit tests.
    Now commenting a line in ut.hs suffices. Before one would need to
    modify distributed-process-test and reinstall it.

--- replicated-log/tests/ut.hs
+++ replicated-log/tests/ut.hs
@@ -2,22 +2,23 @@
 -- Copyright : (C) 2013 Xyratex Technology Limited.
 -- License   : All rights reserved.
 
-import Test (tests)
-
-import Test.Driver
+import qualified Test (tests)
 
 import Control.Concurrent
 import Control.Exception
 import Control.Monad
-import Test.Tasty (testGroup)
+import Test.Tasty
 import Test.Tasty.Environment
+import Test.Tasty.Ingredients.FileReporter
+import Test.Tasty.Ingredients.Basic
+import System.Environment (withArgs)
 
 
 main :: IO ()
 main = do
    (testArgs, runnerArgs) <- parseArgs
    let runWithArgs t r =
-         defaultMainWithArgs (fmap (testGroup "replicated-log") . tests)
+         defaultMainWithArgs (fmap (testGroup "replicated-log") . Test.tests)
                              (testArgs `orDefault` t) (runnerArgs `orDefault` r)
    tid <- myThreadId
    _ <- forkIO $ do threadDelay (30 * 60 * 1000000)
@@ -29,3 +30,15 @@ main = do
 orDefault :: [a] -> [a] -> [a]
 orDefault [] x = x
 orDefault x  _ = x
+
+defaultMainWithArgs :: ([String] -> IO TestTree)
+                    -- ^ Action taking a list of 'String' from stdarg and returning
+                    -- 'Tests's to run.
+                    -> [String]
+                    -- ^ Arguments to pass to tast framework.
+                    -> [String]
+                    -- ^ Arguments to pass to test creation action.
+                    -> IO ()
+defaultMainWithArgs tests runnerArgs testArgs = do
+    tests testArgs >>= withArgs runnerArgs .
+      defaultMainWithIngredients [fileTestReporter [consoleTestReporter]]
