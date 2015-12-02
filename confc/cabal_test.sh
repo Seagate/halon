01f22fb706fd6c3166cb4beaf1ed2647c1b0921e
Alexander Vershilov <alexander.vershilov@tweag.io>
2015-12-02 12:43:52 +0000

    Run all confc tests.

--- confc/cabal_test.sh
+++ confc/cabal_test.sh
@@ -2,10 +2,19 @@
 CONFC_TOP=`pwd`
 BUILD_DIR=${CONFC_TOP}/$1
 TEST_DIR=$(dirname $BUILD_DIR)/test
+DEV=$(sudo lctl list_nids | head -1)
 
 mkdir -p $TEST_DIR
 cd $TEST_DIR
 
 SUDO="sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 $SUDO sh -c \
-   "( cd /tmp && $BUILD_DIR/testconf/testconf `sudo lctl list_nids | head -1`:12345:35:401 `sudo lctl list_nids | head -1`:12345:44:101 )" > testconfc_out.txt
+   "( cd /tmp && $BUILD_DIR/unit-tests/unit-tests )" > unit-tests.txt || exit 1
+$SUDO sh -c \
+   "( cd /tmp && $BUILD_DIR/testconf/testconf $DEV:12345:35:401 $DEV:12345:44:101 )" > testconfc_out.txt || exit 1
+$SUDO sh -c \
+   "( cd /tmp && $BUILD_DIR/conf-test/conf-test $DEV:12345:35:401 $DEV:12345:44:101 $DEV:12345:41:201 )" > conftest_out.txt || exit 1
+$SUDO sh -c \
+   "( cd /tmp && $BUILD_DIR/testspiel/testspiel $DEV:12345:35:401 $DEV:12345:44:101 $DEV:12345:41:201 )" > testspiel_out.txt || exit 1
+$SUDO sh -c \
+   "( cd /tmp && $BUILD_DIR/testcopyconf/testcopyconf $DEV:12345:35:401 $DEV:12345:44:101 $DEV:12345:41:201 )" > testcopyconf_out.txt || exit 1
