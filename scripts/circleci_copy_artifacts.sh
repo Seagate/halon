18d5f072fae6748ea02391ec70866956b50737b6
Mathieu Boespflug <m@tweag.io>
2015-12-03 17:18:39 +0100

    Attempt to speed up (and simplify) circleci_copy_artifacts.sh.
    It's currently adding about 5 mins to the build.

--- scripts/circleci_copy_artifacts.sh
+++ scripts/circleci_copy_artifacts.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-mkdir artifacts
-mkdir artifacts/logs
-find . -name test_output | while read dir ; do \
-  target="artifacts/$(echo $dir | cut --delimiter="/" --fields=2)"; \
-  mkdir "$target"; \
-  cp -av "$dir" "$target" ; \
+# Truncate log file.
+cat < /dev/null > ${CIRCLE_ARTIFACTS}/tests.log
+
+find . -path '*/test_output/*' | while read file; do
+    echo Copying "$file" ...
+    echo TEST "$file" >> ${CIRCLE_ARTIFACTS}/tests.log
+    cat < "$file" >> ${CIRCLE_ARTIFACTS}/tests.log
+    echo >> ${CIRCLE_ARTIFACTS}/tests.log
 done
-find . -type f -name \*.log \( -regex '.*/artifacts.*' -prune -o -print \) | while read l ; do \
-  cp -v "$l" artifacts/logs/ ; \
+
+find . -type f -name \*.log | while read file; do
+    cp -v "$file" ${CIRCLE_ARTIFACTS}
 done
-
-renameFiles() {
-  ls -A "$1" | while read l ; do \
-    n=$(echo $l | sed s/[^-a-zA-Z0-9/.]/_/g)
-    if [[ "$n" != "$l" ]] ; then
-      while test -f "$1/$n" ; do
-        n=${n}_
-      done
-      mv -v "$1/$l" "$1/$n"
-    fi
-    if [[ -d "$1/$n" ]] ; then
-        renameFiles "$1/$n"
-    fi
-  done
-}
-
-renameFiles artifacts
-
-cp -r artifacts ${CIRCLE_ARTIFACTS}
