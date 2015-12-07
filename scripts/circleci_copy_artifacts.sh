9960d8a6aa3704b5868b91fd9d4fc28ed85a430d
Facundo Domínguez <facundo.dominguez@tweag.io>
2015-12-07 11:35:45 -0300

    Surround CIRCLE_ARTIFACTS with double quotes.

--- scripts/circleci_copy_artifacts.sh
+++ scripts/circleci_copy_artifacts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 find . -name test_output | while read file; do
-    cp -rv "$file"/* ${CIRCLE_ARTIFACTS}
+    cp -rv "$file"/* "${CIRCLE_ARTIFACTS}"
 done
 
 find . -type f -name \*.log | while read file; do
-    cp -v "$file" ${CIRCLE_ARTIFACTS}
+    cp -v "$file" "${CIRCLE_ARTIFACTS}"
 done
 
-tar czf logs.tar.gz ${CIRCLE_ARTIFACTS}
-mv logs.tar.gz ${CIRCLE_ARTIFACTS}
+tar czf logs.tar.gz "${CIRCLE_ARTIFACTS}"
+mv logs.tar.gz "${CIRCLE_ARTIFACTS}"
