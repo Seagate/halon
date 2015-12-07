78f9350da4ae0cf27158c36b0b3107f245218916
Facundo Domínguez <facundo.dominguez@tweag.io>
2015-12-07 08:17:06 -0300

    Preserve directory structure of logs.
    Otherwise, some files with the same name in different folders would be lost.

--- scripts/circleci_copy_artifacts.sh
+++ scripts/circleci_copy_artifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find . -path '*/test_output/*' | while read file; do
-    cp -v "$file" ${CIRCLE_ARTIFACTS}
+find . -name test_output | while read file; do
+    cp -rv "$file/*" ${CIRCLE_ARTIFACTS}
 done
 
 find . -type f -name \*.log | while read file; do
