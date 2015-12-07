750f599c86a48d573292b04461b1e5ee3ba207d6
Facundo Domínguez <facundominguez@gmail.com>
2015-12-07 09:33:21 -0300

    fixup: preserve directory structure

--- scripts/circleci_copy_artifacts.sh
+++ scripts/circleci_copy_artifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 find . -name test_output | while read file; do
-    cp -rv "$file/*" ${CIRCLE_ARTIFACTS}
+    cp -rv "$file"/* ${CIRCLE_ARTIFACTS}
 done
 
 find . -type f -name \*.log | while read file; do
