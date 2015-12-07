3833ea38428e8a4de8155d06b949ce493f95ab72
Facundo Domínguez <facundo.dominguez@tweag.io>
2015-12-07 08:10:58 -0300

    Remove unnecessary truncating of logs.

--- scripts/circleci_copy_artifacts.sh
+++ scripts/circleci_copy_artifacts.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Truncate log file.
-cat < /dev/null > ${CIRCLE_ARTIFACTS}/tests.log
-
 find . -path '*/test_output/*' | while read file; do
     cp -v "$file" ${CIRCLE_ARTIFACTS}
 done
