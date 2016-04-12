d46492a906e5f626f7bf85ad665940d30a977657
Facundo Domínguez <facundo.dominguez@tweag.io>
2016-04-12 11:52:44 -0300

    mero-halon: Don't use removed HA.Replicator.Mock.__remoteTable.

--- mero-halon/tests/RemoteTables.hs
+++ mero-halon/tests/RemoteTables.hs
@@ -13,11 +13,11 @@ import Control.Distributed.Process ( RemoteTable )
 import Control.Distributed.Process.Node ( initRemoteTable )
 
 #ifdef USE_MOCK_REPLICATOR
-import HA.Replicator.Mock ( __remoteTable, __remoteTableDecl )
+import HA.Replicator.Mock ( __remoteTableDecl )
 
 
 remoteTable :: RemoteTable
-remoteTable = __remoteTable $ __remoteTableDecl $ haRemoteTable $ meroRemoteTable initRemoteTable
+remoteTable = __remoteTableDecl $ haRemoteTable $ meroRemoteTable initRemoteTable
 #else
 remoteTable :: RemoteTable
 remoteTable = haRemoteTable $ meroRemoteTable initRemoteTable
