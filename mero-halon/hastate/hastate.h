6e3264c99c722fd2f323e3f1246f3ce7adb8e5f0
Maxim Medved <mmedved.ua@gmail.com>
2016-06-05 16:44:01 +0300

    hastate: rename entrypoint-related identifiers
    ha_entrypont_* is renamed to ha_old_entrypoint_* to simplify landing
    of the new notification interface to Mero master
    
    References: https://jira.xyratex.com/browse/MERO-1665

--- mero-halon/hastate/hastate.h
+++ mero-halon/hastate/hastate.h
@@ -41,7 +41,7 @@ typedef struct ha_state_callbacks {
          * Called when a request to read confd and rm service state is received.
          * \return 0 if the request was accepted negative value otherwise
          */
-	int (*ha_state_entrypoint)(struct m0_fom *fom, struct m0_ha_entrypoint_rep *rep_fop);
+	int (*ha_state_entrypoint)(struct m0_fom *fom, struct m0_ha_old_entrypoint_rep *rep_fop);
 } ha_state_callbacks_t;
 
 /**
@@ -70,9 +70,9 @@ int ha_state_notify( rpc_endpoint_t *ep, char *remote_address
                    , struct m0_ha_nvec *note, int timeout_s
                    );
 
-// Populates m0_ha_entrypoint_rep structure by values from haskell world.
+// Populates m0_ha_old_entrypoint_rep structure by values from haskell world.
 void ha_entrypoint_reply_wakeup( struct m0_fom               *fom
-                               , struct m0_ha_entrypoint_rep *rep_fop
+                               , struct m0_ha_old_entrypoint_rep *rep_fop
                                , const int                    rc
                                , const int                    confd_fid_size
                                , const struct m0_fid         *confd_fid_data
