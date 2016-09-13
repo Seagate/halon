4de812ad13eb35626df506a267c369ff4c5a2ce2
Alexander Vershilov <alexander.vershilov@tweag.io>
2016-09-13 06:21:25 +0000

    Adjust tests.

--- mero-halon/tests/HA/RecoveryCoordinator/Mero/Tests.hs
+++ mero-halon/tests/HA/RecoveryCoordinator/Mero/Tests.hs
@@ -275,6 +275,8 @@ testRCsyncToConfd transport pg = do
     let mockMeroConf = MeroConf (testListenName++"@tcp:12345:34:101")
                                 (Fid 0x7000000000000001 0x1)
                                 (Fid 0x7200000000000001 0x18)
+                                (Fid 0x7300000000000001 0x25)
+                                (Fid 0x7300000000000001 0x26)
                                 (_hv_keepalive_frequency defaultHalonVars)
                                 (_hv_keepalive_timeout defaultHalonVars)
                                 (MeroKernelConf UUID.nil)
