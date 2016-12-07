a98a85f96f4ddf69a470c439a8f30e74bf05bf44
Alexander Vershilov <alexander.vershilov@tweag.io>
2016-12-07 20:44:13 +0000

    fixup! Hide internal symbols in EQTracker.

--- halon/src/lib/HA/Startup.hs
+++ halon/src/lib/HA/Startup.hs
@@ -13,6 +13,7 @@ module HA.Startup where
 import HA.RecoverySupervisor ( recoverySupervisor )
 import HA.EventQueue ( EventQueue, eventQueueLabel, startEventQueue, emptyEventQueue )
 import qualified HA.EQTracker as EQT
+import qualified HA.EQTracker.Internal as EQT
 import HA.Logger
 import HA.Multimap ( MetaInfo, defaultMetaInfo, StoreChan )
 import HA.Multimap.Implementation ( Multimap, fromList )
