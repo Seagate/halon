fb0d0e9bb9b54b7b77826fd54bd3bfd23db5bf72
Nicholas Clarke <nicholas.clarke@seagate.com>
2017-05-26 12:03:39 +0100

    Address review (style) comments.

--- mero-halon/src/lib/HA/Services/SSPL/HL/CEP.hs
+++ mero-halon/src/lib/HA/Services/SSPL/HL/CEP.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE GADTs             #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
 -- |
 -- Module    : HA.Services.SSPL.HL.CEP
 -- Copyright : (C) 2015-2017 Seagate Technology Limited.
@@ -11,29 +12,31 @@ module HA.Services.SSPL.HL.CEP
   ( ssplHLRules
   ) where
 
-import           Data.Foldable (forM_)
-import Data.Maybe (catMaybes)
-import qualified Data.Text as T
-import           Data.UUID (toString)
-import qualified HA.Aeson as Aeson
+import           Data.Foldable                           (forM_)
+import           Data.Maybe                              (catMaybes)
+import qualified Data.Text                               as T
+import           Data.UUID                               (toString)
+import qualified HA.Aeson                                as Aeson
 import           HA.EventQueue
-import HA.RecoveryCoordinator.Actions.Hardware
-import           HA.RecoveryCoordinator.Actions.Mero (getClusterStatus)
+import           HA.RecoveryCoordinator.Actions.Hardware
+import           HA.RecoveryCoordinator.Actions.Mero     (getClusterStatus)
 import           HA.RecoveryCoordinator.RC.Actions
-import qualified HA.RecoveryCoordinator.RC.Actions.Log as Log
-import qualified HA.ResourceGraph as G
+import qualified HA.RecoveryCoordinator.RC.Actions.Log   as Log
+import qualified HA.ResourceGraph                        as G
 import           HA.Resources
 import           HA.Resources.Castor
 import           HA.Resources.Mero
-import           HA.Resources.Mero.Note (getState)
-import           HA.Service (getInterface)
+import           HA.Resources.Mero.Note                  (getState)
+import           HA.Service                              (getInterface)
 import           HA.Service.Interface
-import HA.Services.SSPL.LL.CEP
-import HA.Services.SSPL.LL.Resources
-import           HA.Services.SSPLHL (sspl, SsplHlToSvc(..), SsplHlFromSvc(..))
+import           HA.Services.SSPL.LL.CEP
+import           HA.Services.SSPL.LL.Resources
+import           HA.Services.SSPLHL                      (SsplHlFromSvc (..),
+                                                          SsplHlToSvc (..),
+                                                          sspl)
 import           Network.CEP
 import           SSPL.Bindings
-import           Text.Regex.TDFA ((=~))
+import           Text.Regex.TDFA                         ((=~))
 
 -- | Set of SSPL HL rules. Contain rules for status queries inside a graph.
 ssplHLRules :: Definitions RC ()
@@ -46,8 +49,8 @@ ssplHLRules = do
           let
             items = case et of
               Aeson.String "cluster" -> clusterStatus rg
-              Aeson.String "node" -> hostStatus rg (fmap T.unpack mef)
-              _ -> []
+              Aeson.String "node"    -> hostStatus rg (fmap T.unpack mef)
+              _                      -> []
             msg = CommandResponseMessage {
                   commandResponseMessageStatusResponse = Just items
                 , commandResponseMessageResponseId = msgId
@@ -66,7 +69,7 @@ ssplHLRules = do
         command = commandRequestMessageNodeStatusChangeRequestCommand sr
         nodeFilter = case commandRequestMessageNodeStatusChangeRequestNodes sr of
           Just foo -> T.unpack foo
-          Nothing -> "."
+          Nothing  -> "."
       in do
         rg <- getLocalGraph
         let nodes = [ n | n <- G.connectedTo Cluster Has rg
