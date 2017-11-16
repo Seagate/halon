224c46eeaaf212d65c8dd5e0d028bc100fba63e7
Valery V. Vorotyntsev <valery.vorotyntsev@seagate.com>
2017-11-16 13:19:22 +0200

    XXX Castor.Tests: Fix formatting
    Minor edit.

--- mero-halon/tests/HA/Castor/Tests.hs
+++ mero-halon/tests/HA/Castor/Tests.hs
@@ -403,23 +403,20 @@ runGet :: forall app g a. (Application app, g ~ GlobalState app)
        => g -> PhaseM app (Maybe a) a -> Process a
 runGet = runPhaseGet
 
-goRack :: forall l. CI.Rack_XXX0
-       -> PhaseM RC l ()
+goRack :: forall l. CI.Rack_XXX0 -> PhaseM RC l ()
 goRack CI.Rack_XXX0{..} = let rack = R.Rack_XXX1 rack_idx_XXX0 in do
   registerRack rack
   mapM_ (goEnc rack) rack_enclosures_XXX0
-goEnc :: forall l. R.Rack_XXX1
-      -> CI.Enclosure_XXX0
-      -> PhaseM RC l ()
+
+goEnc :: forall l. R.Rack_XXX1 -> CI.Enclosure_XXX0 -> PhaseM RC l ()
 goEnc rack (CI.Enclosure_XXX0{..}) = let
     enclosure = R.Enclosure_XXX1 enc_id_XXX0
   in do
     registerEnclosure rack enclosure
     mapM_ (registerBMC enclosure) enc_bmc_XXX0
     mapM_ (goHost enclosure) enc_hosts_XXX0
-goHost :: forall l. R.Enclosure_XXX1
-       -> CI.Host_XXX0
-       -> PhaseM RC l ()
+
+goHost :: forall l. R.Enclosure_XXX1 -> CI.Host_XXX0 -> PhaseM RC l ()
 goHost enc CI.Host_XXX0{..} = let
     host = R.Host_XXX1 $ T.unpack h_fqdn_XXX0
   in do
