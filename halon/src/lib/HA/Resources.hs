f46e4d170ed7ca9a2365f1585c7f825cec0112a5
Valery V. Vorotyntsev <valery.vorotyntsev@seagate.com>
2017-12-26 11:24:47 +0200

    halon (HA.Resources): use GeneralizedNewtypeDeriving
    - `GeneralizedNewtypeDeriving` extension is already enabled in halon.cabal,
      there's no need to specify it in the .hs file explicitly.
    - Derive `Hashable` and `ToJSON` for newtypes automatically and remove
      `instance` definitions.

--- halon/src/lib/HA/Resources.hs
+++ halon/src/lib/HA/Resources.hs
@@ -8,7 +8,6 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module HA.Resources
   ( Cluster(..)
@@ -36,26 +35,27 @@ import HA.SafeCopy
 
 -- | The root of the resource graph.
 data Cluster = Cluster
-  deriving (Eq, Ord, Show, Typeable, Generic)
+  deriving (Eq, Generic, Ord, Show, Typeable)
+
 instance Hashable Cluster
+instance ToJSON Cluster
+
 storageIndex ''Cluster "67850c56-c077-4e43-a985-310bdea0b4a1"
 deriveSafeCopy 0 'base ''Cluster
-instance ToJSON Cluster
 
 -- | A resource graph representation for nodes.
 newtype Node_XXX2 = Node_XXX2 NodeId
-  deriving (Eq, Ord, Show, Typeable, Generic, Hashable)
-instance ToJSON Node_XXX2
-instance FromJSON Node_XXX2
+  deriving (Eq, Generic, Hashable, Ord, Show, Typeable, FromJSON, ToJSON)
+
 storageIndex ''Node_XXX2 "43ab6bb3-5bfe-4de8-838d-489584b1456c"
 deriveSafeCopy 0 'base ''Node_XXX2
 
 -- | An identifier for epochs.
 newtype EpochId = EpochId Word64
-  deriving (Eq, Ord, Show, Typeable, Generic, Hashable)
+  deriving (Eq, Generic, Hashable, Ord, Show, Typeable, ToJSON)
+
 storageIndex ''EpochId "8c4d4b29-0c24-4bc8-8ab5-e6b3a1f2cc96"
 deriveSafeCopy 0 'base ''EpochId
-instance ToJSON EpochId
 
 --------------------------------------------------------------------------------
 -- Relations                                                                  --
@@ -64,20 +64,29 @@ instance ToJSON EpochId
 -- | A relation connecting the cluster to global resources, such as nodes and
 -- epochs.
 data Has = Has
-  deriving (Eq, Ord, Show, Typeable, Generic)
+  deriving (Eq, Generic, Ord, Show, Typeable)
 
 instance Hashable Has
+instance ToJSON Has
+
 storageIndex ''Has "c912f510-1829-4df0-873d-4a960ff1ff4e"
 deriveSafeCopy 0 'base ''Has
-instance ToJSON Has
 
 -- | A relation connecting a node to the services it runs.
 data Runs = Runs
-  deriving (Eq, Show, Typeable, Generic)
-storageIndex ''Runs "8a53e367-8746-4814-aa3e-fb29c5432119"
-deriveSafeCopy 0 'base ''Runs
+  deriving (Eq, Generic, Show, Typeable)
+
 instance Hashable Runs
 instance ToJSON Runs
+
+storageIndex ''Runs "8a53e367-8746-4814-aa3e-fb29c5432119"
+deriveSafeCopy 0 'base ''Runs
+
+-- | Sent when a node goes down and we need to try to recover it
+newtype RecoverNode = RecoverNode Node_XXX2
+  deriving (Eq, Generic, Ord, Show, Typeable)
+
+deriveSafeCopy 0 'base ''RecoverNode
 
 --------------------------------------------------------------------------------
 -- Dictionaries                                                               --
@@ -91,6 +100,7 @@ $(mkDicts
   [ (''Cluster, ''Has, ''Node_XXX2)
   , (''Cluster, ''Has, ''EpochId)
   ])
+
 $(mkResRel
   [''Cluster, ''Node_XXX2, ''EpochId, ''Has, ''Runs]
   [ (''Cluster, AtMostOne, ''Has, Unbounded, ''Node_XXX2)
@@ -98,8 +108,3 @@ $(mkResRel
   ]
   []
   )
-
--- | Sent when a node goes down and we need to try to recover it
-newtype RecoverNode = RecoverNode Node_XXX2
-  deriving (Typeable, Generic, Show, Eq, Ord)
-deriveSafeCopy 0 'base ''RecoverNode
