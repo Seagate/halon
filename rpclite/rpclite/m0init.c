3acb9eeb818194fb4e0dabaf46e65d9835b7d6ef
Valery V. Vorotyntsev <valery.vorotyntsev@seagate.com>
2018-04-29 16:45:48 +0300

    rpclite/m0init.[hc]: Delete unneeded #includes
    + Coding style cleanup.

--- rpclite/rpclite/m0init.c
+++ rpclite/rpclite/m0init.c
@@ -1,25 +1,33 @@
 //
-// Copyright (C) 2015 Seagate Technology Limited. All rights reserved.
+// Copyright (C) 2018 Xyratex Technology Limited. All rights reserved.
 //
+
+#define M0_TRACE_SUBSYSTEM M0_TRACE_SUBSYS_HA
+#include "lib/trace.h"
+
 #include "m0init.h"
+
 #include "ha/halon/interface.h"
-#include "lib/memory.h"
-#include "mero/init.h"
-#include "mero/version.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "mero/version.h"        // M0_VERSION_GIT_REV_ID
+#include <stdlib.h>              // getenv
 
-struct m0_halon_interface* m0init_hi = NULL;
+struct m0_halon_interface *m0init_hi = NULL;
 
-int m0_init_wrapper () {
-    return m0_halon_interface_init( &m0init_hi
-                                  , M0_VERSION_GIT_REV_ID
-                                  , M0_VERSION_BUILD_CONFIGURE_OPTS
-                                  , getenv("DISABLE_MERO_COMPAT_CHECK") != NULL
-                                  , NULL);
+int m0_init_wrapper(void)
+{
+	M0_PRE(m0init_hi == NULL);
+	return m0_halon_interface_init(
+		&m0init_hi,
+		M0_VERSION_GIT_REV_ID,
+		M0_VERSION_BUILD_CONFIGURE_OPTS,
+		getenv("DISABLE_MERO_COMPAT_CHECK") != NULL,
+		NULL);
 }
 
-void m0_fini_wrapper() {
-    m0_halon_interface_fini(m0init_hi);
+void m0_fini_wrapper(void)
+{
+	m0_halon_interface_fini(m0init_hi);
+	m0init_hi = NULL;
 }
+
+#undef M0_TRACE_SUBSYSTEM
--- rpclite/rpclite/m0init.h
+++ rpclite/rpclite/m0init.h
@@ -1,12 +1,15 @@
 //
-// Copyright (C) 2015 Seagate Technology Limited. All rights reserved.
+// Copyright (C) 2018 Xyratex Technology Limited. All rights reserved.
 //
 #pragma once
+#ifndef __H0_M0INIT_H__
+#define __H0_M0INIT_H__
 
-#include "config.h"
+struct m0_halon_interface;
 
-extern struct m0_halon_interface* m0init_hi;
+extern struct m0_halon_interface *m0init_hi;
 
-int m0_init_wrapper ();
+int m0_init_wrapper(void);
+void m0_fini_wrapper(void);
 
-void m0_fini_wrapper ();
+#endif  // __H0_M0INIT_H__
