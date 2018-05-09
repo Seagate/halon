70bcf3e3ceb3ce4233da336a208f7f7173bebdf6
Valery V. Vorotyntsev <valery.vorotyntsev@seagate.com>
2018-05-09 22:54:06 +0300

    [HALON-796] confc: Delete tests/ and ctests/
    confc/confc.cabal: Due to `buildable: False` setting, `unit-tests`
    and `test-dump-transaction` components were not compiled for some
    while and their code got rotten.
    
    The unit tests in {,c}tests/ directories tested the code which
    had been removed recently (and that code was removed because it
    wasn't used anywhere in Halon).
    
    Currently `confc` package consists mostly of mero API FFI wrappers.
    There is not much logic to test.  And what is there is tested by
    actual usage of Halon...
    
    If we ever want to unit-test `confc` package, we will write new tests.

--- confc/Setup.hs
+++ confc/Setup.hs
@@ -1,20 +1,2 @@
-import Distribution.PackageDescription
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Program
-import Distribution.Simple.Setup
-import System.Exit (exitWith)
-import System.Process (rawSystem)
-
-main = defaultMainWithHooks $ simpleUserHooks
-    { testHook = confc_test
-    }
-
-confc_test :: Args
-           -> PackageDescription
-           -> LocalBuildInfo
-           -> UserHooks
-           -> TestFlags
-           -> IO ()
-confc_test _ _ lbi _ _ =
-    rawSystem "./cabal_test.sh" [ buildDir lbi ] >>= exitWith
+main = defaultMain
