Seagate/halon

commit a5c4860218c5556abf12a70bca410ccf6c55edc4

mero-halon: s3server start/stop is fixed
There were two issues: one in service code another in
process code. In service code there was ill-heuristic logic
to determine the m0t1fs process (which does not have PID)
and s3server was determined as m0t1fs one. But because
s3server does have nonzero PID (unlike m0t1fs),
servicePidMatches() routine returned False. Now we
just dropped completely that ill-coded detection of m0t1fs
as redundant.

The issue in process code was that it relied only on
M0_CONF_HA_PROCESS_M0D userspace process type which is
not true for s3server case. Now the code is fixed to
be more flexible and work with any userspace process type
except M0MKFS which uses same fids as M0D and must
not spoil M0D's status on cluster 1st bootstrap.

This change depends on c17072 mero fix. c17070 mero fix
is required also for the functionality to work.

mero-halon/src/lib/HA/RecoveryCoordinator/Castor/Service/Rules.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds  #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE CPP #-}
 -- |
 -- Module    : HA.RecoveryCoordinator.Castor.Service.Rules
 -- Copyright : (C) 2016-2017 Seagate Technology Limited.
@@ -10,6 +11,7 @@ module HA.RecoveryCoordinator.Castor.Service.Rules
   ( rules
   ) where
 
+import           Debug.Trace (trace,traceM)
 import           Control.Lens
 import           Control.Monad (void)
 import           Data.Foldable (for_)
@@ -58,29 +60,44 @@ ruleNotificationHandler = define "castor::service::notification-handler" $ do
             Just (s :: M0.Service) | p se && isStateChanged s -> Just (eid, s, typ, st)
             _ -> Nothing
 
+      traceXXX func msg =
+        trace $ "[ruleNotificationHandler." ++ func ++ ":"
+                ++ show (__LINE__ :: Int) ++ "] " ++ msg
+
+      servicePidMatches (HAMsg (ServiceEvent _ _ spid) m) _
+        | traceXXX "servicePidMatches"
+          ("fid=" ++ show (_hm_fid m) ++ " pid=" ++ show spid) False = undefined
       servicePidMatches (HAMsg (ServiceEvent _ _ spid) m) ls =
         let rg = lsGraph ls
-            msd = M0.lookupConfObjByFid (_hm_fid m) (lsGraph ls) :: Maybe M0.Service
+            msd = M0.lookupConfObjByFid (_hm_fid m) rg :: Maybe M0.Service
         in case msd of
              Nothing -> False
              Just srv -> fromMaybe False $ do
+               traceM ("[servicePidMatches:" ++ show (__LINE__ :: Int) ++ "] "
+                       ++ "srv=" ++ show srv)
                p :: M0.Process <- G.connectedFrom M0.IsParentOf srv rg
-               is_m0t1fs <- Just $ all (\s -> M0.s_type s `notElem` [CST_IOS, CST_MDS, CST_CONFD, CST_HA])
-                                       (G.connectedTo p M0.IsParentOf rg)
+               traceM ("[servicePidMatches:" ++ show (__LINE__ :: Int) ++ "] "
+                       ++ "proc=" ++ show p)
                if spid == -1
                then return True -- if message is old and does not contain pid, we accept message.
-               else if is_m0t1fs
-                    then return (spid == 0)
-                    else do M0.PID pid <- G.connectedTo p R.Has rg
-                            return (spid == fromIntegral pid)
+               else do
+                 traceM ("[servicePidMatches:" ++ show (__LINE__ :: Int) ++ "] "
+                         ++ "spid=" ++ show spid)
+                 M0.PID pid <- G.connectedTo p R.Has rg
+                 traceM ("[servicePidMatches:" ++ show (__LINE__ :: Int) ++ "] "
+                         ++ "pid=" ++ show pid)
+                 return (spid == fromIntegral pid)
 
       isServiceOnline = serviceTagged (== TAG_M0_CONF_HA_SERVICE_STARTED) M0.SSOnline
       isServiceStopped = serviceTagged (== TAG_M0_CONF_HA_SERVICE_STOPPED) M0.SSOffline
 
+      startOrStop (HAEvent _ (HAMsg se@(ServiceEvent _ _ _) _)) _ _
+        | trace ("[startOrStop:" ++ show (__LINE__ :: Int) ++ "] " ++ show se) False = undefined
       startOrStop msg@(HAEvent _ v) ls _ = return $
         if servicePidMatches v ls
         then case isServiceOnline msg ls of
-               Nothing -> isServiceStopped msg ls
+               Nothing -> let res = isServiceStopped msg ls
+                          in traceXXX "startOrStop" (show res) res
                Just x -> Just x
         else Nothing
 

mero-halon/src/lib/HA/RecoveryCoordinator/Castor/Process/Rules.hs
@@ -506,9 +506,10 @@ ruleProcessStarting = define "castor::process::starting" $ do
     onlineProc (HAEvent eid (HAMsg (ProcessEvent t pt pid) m)) ls _ = do
       let mpd = M0.lookupConfObjByFid (_hm_fid m) (lsGraph ls)
       return $ case (t, pt, mpd) of
-        (TAG_M0_CONF_HA_PROCESS_STARTING, TAG_M0_CONF_HA_PROCESS_M0D, Just (p :: M0.Process)) | pid /= 0 ->
-          Just (eid, p, M0.PID $ fromIntegral pid)
+        (TAG_M0_CONF_HA_PROCESS_STARTING, TAG_M0_CONF_HA_PROCESS_M0MKFS, _) -> Nothing
         (TAG_M0_CONF_HA_PROCESS_STARTING, TAG_M0_CONF_HA_PROCESS_KERNEL, Just (p :: M0.Process)) ->
+          Just (eid, p, M0.PID $ fromIntegral pid)
+        (TAG_M0_CONF_HA_PROCESS_STARTING, _, Just (p :: M0.Process)) | pid /= 0 ->
           Just (eid, p, M0.PID $ fromIntegral pid)
         _ -> Nothing
 
@@ -575,9 +576,10 @@ ruleProcessOnline = define "castor::process::online" $ do
     startedProc (HAEvent eid (HAMsg (ProcessEvent t pt pid) m)) ls _ = do
       let mpd = M0.lookupConfObjByFid (_hm_fid m) (lsGraph ls)
       return $ case (t, pt, mpd) of
-        (TAG_M0_CONF_HA_PROCESS_STARTED, TAG_M0_CONF_HA_PROCESS_M0D, Just (p :: M0.Process)) | pid /= 0 ->
-          Just (eid, p, M0.PID $ fromIntegral pid)
+        (TAG_M0_CONF_HA_PROCESS_STARTED, TAG_M0_CONF_HA_PROCESS_M0MKFS, _) -> Nothing
         (TAG_M0_CONF_HA_PROCESS_STARTED, TAG_M0_CONF_HA_PROCESS_KERNEL, Just (p :: M0.Process)) ->
+          Just (eid, p, M0.PID $ fromIntegral pid)
+        (TAG_M0_CONF_HA_PROCESS_STARTED, _, Just (p :: M0.Process)) | pid /= 0 ->
           Just (eid, p, M0.PID $ fromIntegral pid)
         _ -> Nothing
 
@@ -613,7 +615,7 @@ ruleProcessStopping = define "castor::process::stopping" $ do
     stoppingProc (HAEvent eid (HAMsg (ProcessEvent t pt pid) meta)) ls _ = do
       let mpd = M0.lookupConfObjByFid (_hm_fid meta) (lsGraph ls)
       return $ case (t, pt, mpd) of
-        (TAG_M0_CONF_HA_PROCESS_STOPPING, TAG_M0_CONF_HA_PROCESS_M0D, Just (p :: M0.Process))
+        (TAG_M0_CONF_HA_PROCESS_STOPPING, _, Just (p :: M0.Process))
           | pid /= 0 && isEphemeral p (lsGraph ls) ->
             Just (eid, p, M0.PID $ fromIntegral pid)
         _ -> Nothing
@@ -667,7 +669,7 @@ ruleProcessStopped = define "castor::process::process-stopped" $ do
         Just pd -> do
           let mpid = G.connectedTo pd Has (lsGraph ls)
           return $ case (t, pt, pd) of
-            (TAG_M0_CONF_HA_PROCESS_STOPPED, TAG_M0_CONF_HA_PROCESS_M0D, p :: M0.Process)
+            (TAG_M0_CONF_HA_PROCESS_STOPPED, _, p :: M0.Process)
                | pid /= 0
                , Just (M0.PID ppid) <- mpid
                , ppid == fromIntegral pid -> Just (eid, p, M0.PID $ fromIntegral pid)

commit 686bd81c1a34d69a23d697fba101edf5d3c261ce

HALON-799: fix clovis-app's service starting
clovis-app is Independent process type and its services
should not be started by halon. The regression appeared
after commit a5c4860 fix for s3server. The fix was (and is)
correct, it just should be extended to check the process
type of the services also before starting/stopping them.

mero-halon/src/lib/HA/RecoveryCoordinator/Castor/Service/Rules.hs
@@ -27,6 +27,7 @@ import qualified HA.ResourceGraph as G
 import           HA.Resources (Has(..))
 import qualified HA.Resources.Mero as M0
 import qualified HA.Resources.Mero.Note as M0
+import qualified HA.Resources.Castor.Initial as CI
 import           Mero.ConfC (ServiceType(..))
 import           Mero.Notification.HAState
   ( HAMsg(..)
@@ -60,16 +61,20 @@ ruleNotificationHandler = define "castor::service::notification-handler" $ do
 
       servicePidMatches (HAMsg (ServiceEvent _ _ spid) m) ls =
         let rg = lsGraph ls
-            msd = M0.lookupConfObjByFid (_hm_fid m) rg :: Maybe M0.Service
-        in case msd of
-             Nothing -> False
-             Just srv -> fromMaybe False $ do
-               p :: M0.Process <- G.connectedFrom M0.IsParentOf srv rg
-               if spid == -1
-               then return True -- if message is old and does not contain pid, we accept message.
-               else do
-                 M0.PID pid <- G.connectedTo p Has rg
-                 return (spid == fromIntegral pid)
+        in fromMaybe False $ do
+            svc :: M0.Service <- M0.lookupConfObjByFid (_hm_fid m) rg
+            proc :: M0.Process <- G.connectedFrom M0.IsParentOf svc rg
+            case (spid, G.connectedTo proc Has rg) of
+              (_, Just (CI.PLClovis _ CI.Independent)) ->
+                  return False  -- Ignore notifications from unmanaged Clovis
+                                -- processes.
+              (-1, _) ->
+                  return True   -- Accept notifications with spid == -1.
+                                -- XXX `spid` is the value returned by
+                                -- getpid(). How can it ever be -1?
+              _ -> do
+                  M0.PID pid <- G.connectedTo proc Has rg
+                  return (spid == fromIntegral pid)
 
       isServiceOnline = serviceTagged (== TAG_M0_CONF_HA_SERVICE_STARTED) M0.SSOnline
       isServiceStopped = serviceTagged (== TAG_M0_CONF_HA_SERVICE_STOPPED) M0.SSOffline